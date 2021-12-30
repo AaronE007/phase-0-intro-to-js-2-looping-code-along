9d43aa83fa5292c21a7f31d8fd190e0c57334975
Aaron Elias <aaronelias007@gmail.com>
2021-12-29 21:12:01 -0500

    commit tree 8b81630376b2fe479f1896e99ff8873fdf8cf21c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let pracArray = [];
+function writeCards(names,events){
+    
+
+    for (let i = 0; i < names.length; i++){
+         let message = (`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+         pracArray.push(message);}
+
+         return pracArray;
+    }
+
+function countDown(integer){
+while (integer >= 0) {
+  console.log(integer--);
+}
+}
